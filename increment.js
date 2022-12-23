f8626f29e4cb7797f0cd5b1e69d4688bf8a1a287
arimura <kotaro_arimura@voyagegroup.com>
2022-12-23 16:18:28 +0900

    init

---
+++ increment.js
@@ -0,0 +1,4 @@
+function incerement(num) {
+    return num + 1;
+}
+console.log(incerement(999));
---
+++ increment.ts
@@ -0,0 +1,5 @@
+function incerement(num :number){
+    return num + 1;
+}
+
+console.log(incerement(999));
