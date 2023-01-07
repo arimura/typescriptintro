f634159934375832fdc303f96163dd26e6fc0cc1
arimura <kotaro_arimura@voyagegroup.com>
2023-01-08 06:37:07 +0900

    In syntax check

--- syntax-test/operators.test.ts
+++ syntax-test/operators.test.ts
@@ -1,7 +1,30 @@
+
+
 describe("Test for in operator", () => {
     test("check property", () => {
-        const obj = {a: 1};
+        const obj = { a: 1 };
         expect('a' in obj).toBeTruthy();
         expect('b' in obj).toBeFalsy();
+    });
+
+    test("check type", () => {
+
+        type User = {
+            name: string;
+            age: number;
+        };
+        const user: User = {
+            name: "John",
+            age: 30,
+        };
+
+        function check(key: keyof User) {
+            if (key in user) {
+                return true;
+            }
+            return false;
+        }
+
+        expect(check("name")).toBeTruthy();
     });
 });
